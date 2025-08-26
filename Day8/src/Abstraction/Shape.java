ac5c8ad6cc281dc8d302290b8ad8098793b0b986
risabhan.a <risabhanhero@gmail.com>
2025-08-26 11:53:21 +0530

    Day 8 has been completed

---
+++ Day8/src/Abstraction/Shape.java
@@ -0,0 +1,11 @@
+package Abstraction;
+
+public abstract class Shape {
+protected float area;
+	
+	abstract void calArea();
+	
+	void show() {
+		System.out.println("Area of shape is "+area);
+	}
+}
