a43bb97928aa08c19380b68b5afc62a70c82ae3d
risabhan.a <risabhanhero@gmail.com>
2025-09-01 15:30:29 +0530

    Day 11 has been updated

---
+++ Day11/src/v1/ArrayOperation.java
@@ -0,0 +1,27 @@
+package v1;
+
+public class ArrayOperation {
+int intArray[] ;
+	
+	public ArrayOperations() {
+		super();
+		
+	}
+
+	public ArrayOperations(int[] intArray) {
+		super();
+		this.intArray = intArray;
+		
+	}
+	
+	public void displayArray() {
+		for(int no:intArray) {
+			System.out.println("The array elements are: "+no);
+		}
+	}
+	
+	public int getElement(int position) {
+		return intArray[position];
+	}
+	
+}
